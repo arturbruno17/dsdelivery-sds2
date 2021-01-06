820cac4f3efaa08d91e313549bdc5cadea2d229b
Artur Bruno <arturjuliao20@gmail.com>
2021-01-06 16:48:58 -0300

    Add ProductCard, ProductsList and API connection

---
+++ front-web/src/Orders/ProductCard.tsx
@@ -0,0 +1,34 @@
+import { Product } from './types'
+
+type Props = {
+  product: Product
+}
+
+function formatPrice(price: number) {
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+
+  return formatter.format(price)
+}
+
+function ProductCard({ product }: Props) {
+  return (
+    <div className="order-card-container">
+      <h3 className="order-card-title">
+        {product.name}
+      </h3>
+      <img className="order-card-image" src={product.imageUri} alt={product.name}/>
+      <h3 className="order-card-price">
+        {formatPrice(product.price)}
+      </h3>
+      <div className="order-card-description">
+        <h3>Descrição</h3>
+        <p>{product.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react'
+import { fetchProducts } from '../api'
+import ProductsList from './ProductsList'
 import StepsHeader from './StepsHeader'
 import './styles.css'
+import { Product } from './types'
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="orders-container">
       <StepsHeader />
+      <ProductsList products={products}/>
     </div>
   )
 }

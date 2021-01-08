e7023a72108f2ef55525549a3ac57dc0800034d3
Artur Bruno <arturjuliao20@gmail.com>
2021-01-08 11:31:15 -0300

    Correcting development error

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -22,7 +22,7 @@ function Orders() {
   useEffect(() => {
     fetchProducts()
       .then(response => setProducts(response.data))
-      .catch(error => console.log(error))
+      .catch(error => toast.warning("Erro ao listar produtos!"))
   }, [])
 
   const handleSelectProduct = (product: Product) => {
